fc8dff0e9ea25bb7a61b6868c71cf9643d26157a
Ruben Pollan <meskio@sindominio.net>
2009-11-18 12:35:10 +0100

    Added file of headers definitions.

---
+++ src/defs.h
@@ -0,0 +1,53 @@
+
+/**************************************************************************
+ * Copyright (C) 2007-2009 Ruben Pollan Bella <meskio@sindominio.net>     *
+ *                                                                        *
+ *  This file is part of TuDu.                                            *
+ *                                                                        *
+ *  TuDu is free software; you can redistribute it and/or modify          *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation; either version 3 of the License.        *
+ *                                                                        *
+ *  TuDu is distributed in the hope that it will be useful,               *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ **************************************************************************/
+
+#ifndef DEFS_H
+#define DEFS_H
+
+using namespace std;
+
+/* system includes */
+#include <ncursesw/ncurses.h>
+#include <form.h>
+#include <string>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <list>
+#include <stack>
+#include <algorithm>
+#include <set>
+#include <ctime>
+#include <cstring>
+#include <csignal>
+#include <cstdio>
+#include <cstdlib>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <locale>
+#include <cwchar>
+#include <clocale>
+#include <climits>
+#include <iostream>
+#include <cerrno>
+
+#endif

11da79361f2e682d63d15b7831711aefc13d927c
Gustavo Pantuza Coelho Pinto <gustavo.pantuza@corp.globo.com>
2017-04-10 18:51:20 -0300

    Added indentation on line comment

--- src/interface.cc
+++ src/interface.cc
@@ -6,7 +6,7 @@
  *                                                                        *
  *  TuDu is free software; you can redistribute it and/or modify          *
  *  it under the terms of the GNU General Public License as published by  *
- *  the Free Software Foundation; version 3 of the License.        *
+ *  the Free Software Foundation; version 3 of the License.               *
  *                                                                        *
  *  TuDu is distributed in the hope that it will be useful,               *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
